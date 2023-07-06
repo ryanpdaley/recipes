ce2a9b45241c19f69f229de8bfd5f68a01a66f38
Ryan Daley <rpdaley@gmail.com>
2023-07-06 13:54:29 -0400

    Make the Ingredients list functional

--- src/components/RecipeBody/components/RecipeIngredients/RecipeIngredients.tsx
+++ src/components/RecipeBody/components/RecipeIngredients/RecipeIngredients.tsx
@@ -1,4 +1,4 @@
-import { VerticalStack, Checkbox, Text } from "@shopify/polaris";
+import { VerticalStack, Text } from "@shopify/polaris";
 import { RecipeIngredientsProps } from "../../../../types";
 import { FRACTION_DICTIONARY } from "../../../../constants";
 
@@ -9,28 +9,61 @@ const convertDecimals = (measurement: number): string => {
     return measurement.toString();
 
   return (
-    (wholeNumber > 0 ? wholeNumber : "") +
-    " " +
+    (wholeNumber > 0 ? wholeNumber + " " : "") +
     String.fromCharCode(FRACTION_DICTIONARY[decimal])
   );
 };
 
-const RecipeIngredients = ({ recipeIngredients }: RecipeIngredientsProps) => {
+const RecipeIngredients = ({
+  recipeIngredients,
+  checkedItems,
+  setCheckedItems,
+}: RecipeIngredientsProps) => {
+  const handleChange = (event: { target: { value: any; checked: any } }) => {
+    const value = event.target.value;
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      setCheckedItems([...checkedItems, value]);
+    } else {
+      const filteredList = checkedItems.filter((item) => item !== value);
+      setCheckedItems(filteredList);
+    }
+  };
+
   return (
     <VerticalStack gap="1">
       <Text variant="headingMd" as="h6">
         Ingredients:
       </Text>
-      {recipeIngredients.map((ingredient) => {
-        const qualifierString =
-          ingredient.qualifierString !== null
-            ? ` (${ingredient.qualifierString})`
-            : "";
-        const label = `${convertDecimals(ingredient.measurement)} ${
-          ingredient.measurementUnit
-        } ${ingredient.name}${qualifierString}`;
-        return <Checkbox label={label} key={ingredient.name} />;
-      })}
+      <table>
+        <tbody>
+          {recipeIngredients.map((ingredient) => {
+            const qualifierString =
+              ingredient.qualifierString !== null
+                ? ` (${ingredient.qualifierString})`
+                : "";
+            const rowItem = `${convertDecimals(ingredient.measurement)} ${
+              ingredient.measurementUnit
+            } ${ingredient.name}`;
+            const label = `${rowItem} ${qualifierString}`;
+            return (
+              <tr key={label}>
+                <td className="recipe-body-ingredient-check">
+                  <input
+                    type="checkbox"
+                    value={rowItem}
+                    onChange={handleChange}
+                  />
+                </td>
+                <td>
+                  <label>{label}</label>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </VerticalStack>
   );
 };
