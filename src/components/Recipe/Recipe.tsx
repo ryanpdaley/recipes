d4ab42b9eddd6ef1e3cabefa58bed36c935c42f4
Ryan Daley <rpdaley@gmail.com>
2023-07-10 10:57:40 -0400

    Another sampleRecipe

--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -2,7 +2,7 @@ import "./Recipe.css";
 import { useEffect, useState } from "react";
 import { Page, AppProvider, Card } from "@shopify/polaris";
 import translations from "./translations/en.json";
-import sampleRecipe from "../../sampleRecipe.json";
+import sampleRecipe from "../../sampleRecipe5.json";
 import RecipeHead from "../RecipeHead/RecipeHead";
 import RecipeBody from "../RecipeBody/RecipeBody";
 
